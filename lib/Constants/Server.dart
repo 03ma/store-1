87ac4cbc457221711d3ddd62ea1aaddf82c22b0e
03ma <ahashow86@gmail.com>
2022-10-10 08:50:22 -0700

    8:50

--- lib/Constants/Server.dart
+++ lib/Constants/Server.dart
@@ -1,4 +1,6 @@
-const url = 'https://www.nazar-store.com';
+const ip = '38.242.249.29';
+const port = '80';
+const url = 'http://$ip:$port';
 const ProductImageUrl = url + "/photo/product/";
 const CategoryImageUrl = url + "/photo/category/";
 const AdvertisementImageUrl = url + '/photo/advertisement/';
